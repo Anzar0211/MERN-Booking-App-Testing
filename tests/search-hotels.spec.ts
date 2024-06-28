4dfb0088eb58d106bbba7933463f3de006e47934
ANZAR <anzarkhan790@gmail.com>
2024-06-28 17:05:28 +0530

    test: Add hotel search test case

---
+++ tests/search-hotels.spec.ts
@@ -0,0 +1,30 @@
+import{test,expect} from "@playwright/test"
+
+
+const UI_URL= "http://localhost:5173/"
+
+
+test.beforeEach(async ({page})=>{
+    await page.goto(UI_URL)
+        //GET SIGN IN BUTTON
+    await page.getByRole("link",{name:"Sign In"}).click();
+    await expect(page.getByRole("heading",{name:"Sign In"})).toBeVisible()
+        //FILL IN EMAIL
+    await page.locator("[name=email]").fill("test_register_45136@gmail.com");
+        //FILL IN PASSWORD
+    await page.locator("[name=password]").fill("test1234");
+
+        //CLICK SIGN IN BUTTON
+    await page.getByRole("button",{name:"Login"}).click()
+
+        //EXPECT TO SEE WELCOME MESSAGE
+    await expect(page.getByText("Login Successful")).toBeVisible()
+})
+
+test("Should Show Hotel Search Results",async({page})=>{
+    await page.goto(`${UI_URL}`);
+    await page.getByPlaceholder("Where are you going?").fill("Gurgaon");
+    await page.getByRole("button",{name:"Search"}).click();
+    await expect(page.getByText("Hotels found in Gurgaon")).toBeVisible()
+    await expect(page.getByText("test hotel 1")).toBeVisible()
+})
